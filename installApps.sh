775f027a112c7932e7bb9b660b860032f0781482
ileslie71 <iaincleslie@googlemail.com>
2017-02-14 21:03:46 +0000

    upload application install script

---
+++ installApps.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#browsers
+brew cask install google-chrome
+brew cask install firefox
+brew cask install opera
+#chat clients
+brew cask install hipchat
+brew cask install skype
+#ide - uncomment the ones you want
+#brew cask install eclipse-ide
+brew cask install sublime-text
+#brew cask install visual-studio-code
+#brew cask install atom
+#testing
+brew cask install postman
+#ftp
+brew cask install filezilla
+#commandbox
+brew install commandbox
+#virtual machines and containers
+brew cask install docker
+brew cask install vagrant
+brew cask install virtualbox
+
